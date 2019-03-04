4e498767c82ed6cffcd4e1304539642421ba1215
Ant Stanley <antstanley@users.noreply.github.com>
2019-03-04 00:12:54 +0000

    update initial page design

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,6 @@
+const MarkdownIt = require('markdown-it');
+const md = new MarkdownIt()
+
 module.exports = function (eleventyConfig) {
     // Copy the `img/` directory
     eleventyConfig.addPassthroughCopy('src/img')
@@ -5,7 +8,7 @@ module.exports = function (eleventyConfig) {
     // Copy the `css/fonts/` directory
     // If you use a subdirectory, it’ll copy using the same directory structure.
     eleventyConfig.addPassthroughCopy('src/css')
-  
+    eleventyConfig.addNunjucksShortcode("markdown", function(text) { return md.render(text) });
     return {
       passthroughFileCopy: true,
       dir: {
