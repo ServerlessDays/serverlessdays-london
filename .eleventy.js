ee17e31bcb54fff111ade7649ce9e313efa30d34
Ant Stanley <antstanley@users.noreply.github.com>
2019-03-15 22:30:32 +0000

    add purgecss to eleventy config to remove unused css and insert inline

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,24 @@ const MarkdownIt = require('markdown-it');
 const md = new MarkdownIt()
 
 const Purgecss = require('purgecss')
+const { JSDOM } = require('jsdom')
+
+const cssFiles = ['./src/css/custom.css','./src/css/markdown.css', './src/css/tachyons.css']
+
+const insertCss = (html, css) => {
+
+    const dom = new JSDOM(html)
+    
+    const { document } = dom.window
+
+    let head = document.getElementsByTagName('head')[0];
+    let style = document.createElement("style");
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+
+    return dom.serialize()
+}
 
 
 module.exports = function (eleventyConfig) {
@@ -10,7 +28,7 @@ module.exports = function (eleventyConfig) {
   
     // Copy the `css/fonts/` directory
     // If you use a subdirectory, it’ll copy using the same directory structure.
-    eleventyConfig.addPassthroughCopy('src/css')
+    // eleventyConfig.addPassthroughCopy('src/css')
     eleventyConfig.addNunjucksShortcode("markdown", function(text) { return md.render(text) });
     eleventyConfig.addFilter('csspurge', function(code) {
       const purgecss = new Purgecss({
@@ -20,6 +38,26 @@ module.exports = function (eleventyConfig) {
       return purgecssResult = purgecss.purge()
     })
     
+    eleventyConfig.addTransform("purgeCSS", function(content, outputPath){
+      if( outputPath.endsWith(".html") ) {
+        console.log(outputPath)
+        const purgecss = new Purgecss({
+          content: [outputPath],
+          css: cssFiles
+        })
+        const purgecssResult = purgecss.purge()
+        let cssMerge = ''
+        if(purgecssResult.length>0){
+          for (let i = 0; i < purgecssResult.length; i++){
+            cssMerge= cssMerge.concat(purgecssResult[i].css)
+          }
+          return insertCss(content, cssMerge)
+        }
+      }
+      return content
+
+    })
+
     return {
       passthroughFileCopy: true,
       dir: {
